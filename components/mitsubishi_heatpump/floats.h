d9b82454e56368251f5aae6f65372cb4712bb1bf
Kevin Dillane <dillane.k@gmail.com>
2024-11-05 10:25:55 -0800

    Quiet floats.h warns

--- components/mitsubishi_heatpump/floats.h
+++ components/mitsubishi_heatpump/floats.h
@@ -5,14 +5,15 @@
 
 namespace devicestate {
 
-    static bool same_float(const float a, const float b, const float epsilon) {
+    __attribute__((unused))static bool same_float(const float a, const float b, const float epsilon) {
         if (std::isnan(a) || std::isnan(b)) {
             return false;
         }
         return std::fabs(a - b) <= ( (std::fabs(a) > std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
     }
 
-    static float clamp(const float value, const float min, const float max) {
+
+    __attribute__((unused))static float clamp(const float value, const float min, const float max) {
         if (value < min) {
             return min;
         } else if (value > max) {
