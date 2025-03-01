e99382ebe8bffb86bf5e83ee51028d82a484659c
Kevin Dillane <dillane.k@gmail.com>
2025-02-28 23:46:27 -0800

    Fix

--- components/mitsubishi_heatpump/pid_workflowstep.h
+++ components/mitsubishi_heatpump/pid_workflowstep.h
@@ -16,6 +16,7 @@ namespace workflow {
     
             PIDController *pidController;
 
+            bool getOffsetDirection(const DeviceState* deviceState);
             void ensurePIDTarget(devicestate::DeviceStateManager* deviceManager);
         
         public:
